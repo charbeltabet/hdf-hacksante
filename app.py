5f80c98ead74fea584e88af5db5ca4ccf2fe3844
Charbel Tabet <charbeltabet@live.com>
2026-02-05 13:47:48 +0200

    add docker

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def submit():
         return jsonify({'success': False, 'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
